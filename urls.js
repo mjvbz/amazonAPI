06c88989f1c90905395baafbbd29376895c7dcb2
รɦσɳ รαɳƭɦσรɦ <shonsanthosh1977@gmail.com>
2021-12-07 10:20:20 +0530

    Create urls.js

---
+++ urls.js
@@ -0,0 +1,4 @@
+`${baseUrl}&url=https://www.amazon.com/dp/${productId}`)
+`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`);
+`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`);
